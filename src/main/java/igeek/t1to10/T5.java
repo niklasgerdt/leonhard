f82dd8d4e0c1350e4e900f42a24039ac83ba44f2
Niklas Gerdt <gerdtni@gmail.com>
2014-11-12 09:39:42 +0200

    T13

--- src/main/java/igeek/t1to10/T5.java
+++ src/main/java/igeek/t1to10/T5.java
@@ -1,20 +1,18 @@
 package igeek.t1to10;
 
 import java.util.stream.Stream;
-
 import static java.lang.System.out;
 
 public class T5 {
-    public static void main(String[] args) {
-        new T5();
-    }
+	public static void main(String[] args) {
+		new T5();
+	}
 
-    public T5() {
-        Stream.iterate(1, a -> a + 1)
-                .filter(a -> (a % 20 == 0 && a % 19 == 0 && a % 18 == 0 && a % 17 == 0 && a % 16 == 0 && a % 15 == 0 && a % 14 == 0 && a % 13 == 0))
-                .filter(a -> (a % 12 == 0 && a % 11 == 0))
-                .limit(1)
-                .forEach(a -> out.println(a));
-    }
-
+	public T5() {
+		Stream.iterate(1, a -> a + 1)
+				.filter(a -> (a % 20 == 0 && a % 19 == 0 && a % 18 == 0 && a % 17 == 0 && a % 16 == 0 && a % 15 == 0 && a % 14 == 0 && a % 13 == 0))
+				.filter(a -> (a % 12 == 0 && a % 11 == 0))
+				.limit(1)
+				.forEach(a -> out.println(a));
+	}
 }
--- src/main/java/igeek/t1to10/T6.java
+++ src/main/java/igeek/t1to10/T6.java
@@ -1,19 +1,22 @@
 package igeek.t1to10;
 
 import java.util.stream.Stream;
-
 import static java.lang.System.out;
 
 public class T6 {
-    public static void main(String[] args) {
-        new T6();
-    }
+	public static void main(String[] args) {
+		new T6();
+	}
 
-    public T6() {
-        int sum = Stream.iterate(1, a -> a + 1).limit(100).reduce(0, (a, b) -> a + (b * b));
-        int sqr = Stream.iterate(1, a -> a + 1).limit(100).reduce(0, (a, b) -> a + b);
-        sqr = sqr * sqr;
+	public T6() {
+		int sum = Stream.iterate(1, a -> a + 1)
+				.limit(100)
+				.reduce(0, (a, b) -> a + (b * b));
+		int sqr = Stream.iterate(1, a -> a + 1)
+				.limit(100)
+				.reduce(0, (a, b) -> a + b);
+		sqr = sqr * sqr;
 
-        out.println(sum + ", " + sqr + ", " + (sqr - sum));
-    }
+		out.println(sum + ", " + sqr + ", " + (sqr - sum));
+	}
 }
