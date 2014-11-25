19d0454056d4072fb02f1476c0a7a8edaa1dd47a
Niklas Gerdt <gerdtni@gmail.com>
2014-11-25 23:13:49 +0200

    16

---
+++ src/main/java/igeek/t11to20/t16.java
@@ -0,0 +1,17 @@
+package igeek.t11to20;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class t16 {
+    public static void main(String[] args) {
+        BigInteger b = new BigInteger("2");
+        BigInteger d = b.pow(1000);
+        System.out.println(d);
+        long l = d.toString()
+                .chars()
+                .reduce(0, (i, j) -> i + Character.getNumericValue(j));
+        System.out.println(l);
+    }
+
+}
