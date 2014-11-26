d9a62885fa112fffbd257e5a53207e6db48a59bd
Niklas Gerdt <gerdtni@gmail.com>
2014-11-26 13:24:51 +0200

    25

---
+++ src/main/java/igeek/t11to20/t25.java
@@ -0,0 +1,29 @@
+package igeek.t11to20;
+
+import java.math.BigInteger;
+import java.util.stream.Stream;
+
+public class t25 {
+
+    public static void main(String[] a) {
+        Stream.iterate(new Fib(2, new BigInteger("1"), new BigInteger("1")), fib -> {
+            BigInteger c = fib.a.add(fib.b);
+            return new Fib(fib.term + 1, fib.b, c);
+        })
+                .filter(s -> s.b.toString().length() >= 1000)
+                .limit(1)
+                .forEach(s -> System.out.println(s.term));
+    }
+
+    static class Fib {
+        final long term;
+        final BigInteger a;
+        final BigInteger b;
+
+        Fib(long term, BigInteger a, BigInteger b) {
+            this.term = term;
+            this.a = a;
+            this.b = b;
+        }
+    }
+}
