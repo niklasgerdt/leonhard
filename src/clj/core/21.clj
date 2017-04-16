cee8fdcb902a61890c33645ad2c3cc174ba917b2
Niklas Gerdt <niklas.gerdt@zervant.com>
2017-04-16 15:31:41 +0300

    21

--- src/clj/core/21.clj
+++ src/clj/core/21.clj
@@ -1,35 +1,33 @@
 (ns core.21)
 
-(def i (range 1 1000))
-
-(defn  divs [div nom col]
-  (if (>= div nom)
+(defn divs [div nom col]
+  "returns proper dividers as seq
+  params divider, nominator, seq of dividers"
+  (if (> div nom)
     col
     (if (= 0 (mod nom div))
       (recur (inc div) nom (conj col div))
       (recur (inc div) nom col))))
 
-(defn sum [x]
-  (reduce + x))
+(defn int-dividers-map [s  m]
+  "finds sums of proper dividers and returns map of int -> sum(dividers)"
+  (if (empty? s)
+    m 
+    (let [f (first s)
+          r (rest s)
+          z (reduce + (divs 1 f []))
+          mz (conj m {f z})]
+      (recur r mz))))
 
-(defn logic [x c]
-  (if (empty? x)
-    c
-    (let [a (first x)
-          b (rest x)]
-      (recur b (conj {x (reduce + (divs 1 x []))})))))
+(defn amicable-numbers [m v]
+  "amicable numbers as vector from map"
+  (if (empty? m)
+    v
+    (let [y (key (first m))
+          z (get m y)
+          x (get m z)
+          r (dissoc m y)]
+      (if (and (= y x) (not= y z))
+        (recur r (conj v y z))
+        (recur r v)))))
 
-(defn sums [x s]
-  (if (empty? x)
-    s
-    (let [y (get x (first x))
-          z (get x y)]
-      (if (= y z)
-        (recur (rest x) (+ s y))
-        (recur (rest x) s))
-
-    (if (= (get x (first x)) 
-
-(defn -main []
-
-  (logic (range 1 10000) {})
